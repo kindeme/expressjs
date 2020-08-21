f8803fdbe3f2b80730c242d3048190ac2901f404
Kindeme <fkounoudji@gmail.com>
2020-08-21 16:08:23 -0500

    Passport JWT

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -16,7 +16,7 @@ promotionRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		Promotion.create(req.body)
 			.then((promotion) => {
 				console.log("Partner Created ", promotion);
@@ -26,11 +26,11 @@ promotionRouter
 			})
 			.catch((err) => next(err));
 	})
-	.put((req, res) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /partners");
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Promotion.deleteMany()
 			.then((response) => {
 				res.statusCode = 200;
@@ -51,13 +51,13 @@ promotionRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post((req, res) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end(
 			`POST operation not supported on /promotions/${req.params.promotionId}`
 		);
 	})
-	.put((req, res, next) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		Promotion.findByIdAndUpdate(
 			req.params.promotionId,
 			{
@@ -72,7 +72,7 @@ promotionRouter
 			})
 			.catch((err) => next(err));
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Promotion.findByIdAndDelete(req.params.promotionId)
 			.then((response) => {
 				res.statusCode = 200;
